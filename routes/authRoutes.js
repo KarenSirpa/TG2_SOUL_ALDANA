079c0b4283046cd103e449d7bae873bcc401eaad
yayoneitor <132401683+yayoneitor@users.noreply.github.com>
2025-10-03 10:39:17 -0400

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-console.log('Tipo de authController.login:', typeof authController.login); // Esta línea
-router.post('/login', authController.login);
-
-module.exports = router;
